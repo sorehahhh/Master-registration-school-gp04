109e1a8511d7ad9516191b04bba5a561faba620d
sorehahhh <78630154+sorehahhh@users.noreply.github.com>
2021-10-01 17:38:53 +0800

    update

--- ProjectDemoCRS1.1/ProjectDemoCRS/StudentInformation/StudentListForm.Designer.vb
+++ ProjectDemoCRS1.1/ProjectDemoCRS/StudentInformation/StudentListForm.Designer.vb
@@ -33,8 +33,26 @@ Partial Class StudentListForm
         Me.ICButton = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.searchTextBox = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dobDateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.nameTextBox = New System.Windows.Forms.TextBox()
+        Me.PhoneNumberTextBox = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.icNumberTextBox = New System.Windows.Forms.TextBox()
+        Me.matricTextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PreviousButton = New System.Windows.Forms.Button()
+        Me.AddNewButton = New System.Windows.Forms.Button()
+        Me.NextButton = New System.Windows.Forms.Button()
+        Me.SaveButton = New System.Windows.Forms.Button()
+        Me.DeleteButton = New System.Windows.Forms.Button()
+        Me.CloseButton = New System.Windows.Forms.Button()
         CType(Me.StudentDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Search.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'updateStudentButton
@@ -77,11 +95,11 @@ Partial Class StudentListForm
         '
         Me.StudentDataGridView.BackgroundColor = System.Drawing.Color.Turquoise
         Me.StudentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.StudentDataGridView.Location = New System.Drawing.Point(39, 119)
+        Me.StudentDataGridView.Location = New System.Drawing.Point(504, 135)
         Me.StudentDataGridView.MultiSelect = False
         Me.StudentDataGridView.Name = "StudentDataGridView"
         Me.StudentDataGridView.RowHeadersWidth = 49
-        Me.StudentDataGridView.Size = New System.Drawing.Size(1242, 338)
+        Me.StudentDataGridView.Size = New System.Drawing.Size(503, 338)
         Me.StudentDataGridView.TabIndex = 9
         '
         'Search
@@ -143,12 +161,173 @@ Partial Class StudentListForm
         Me.searchTextBox.Size = New System.Drawing.Size(295, 32)
         Me.searchTextBox.TabIndex = 0
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CloseButton)
+        Me.GroupBox1.Controls.Add(Me.DeleteButton)
+        Me.GroupBox1.Controls.Add(Me.SaveButton)
+        Me.GroupBox1.Controls.Add(Me.NextButton)
+        Me.GroupBox1.Controls.Add(Me.AddNewButton)
+        Me.GroupBox1.Controls.Add(Me.PreviousButton)
+        Me.GroupBox1.Controls.Add(Me.dobDateTimePicker1)
+        Me.GroupBox1.Controls.Add(Me.nameTextBox)
+        Me.GroupBox1.Controls.Add(Me.PhoneNumberTextBox)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.icNumberTextBox)
+        Me.GroupBox1.Controls.Add(Me.matricTextBox)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 135)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(486, 321)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'dobDateTimePicker1
+        '
+        Me.dobDateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dobDateTimePicker1.Location = New System.Drawing.Point(118, 108)
+        Me.dobDateTimePicker1.Name = "dobDateTimePicker1"
+        Me.dobDateTimePicker1.Size = New System.Drawing.Size(260, 20)
+        Me.dobDateTimePicker1.TabIndex = 31
+        '
+        'nameTextBox
+        '
+        Me.nameTextBox.Location = New System.Drawing.Point(119, 79)
+        Me.nameTextBox.Name = "nameTextBox"
+        Me.nameTextBox.Size = New System.Drawing.Size(262, 20)
+        Me.nameTextBox.TabIndex = 30
+        '
+        'PhoneNumberTextBox
+        '
+        Me.PhoneNumberTextBox.Location = New System.Drawing.Point(119, 140)
+        Me.PhoneNumberTextBox.Name = "PhoneNumberTextBox"
+        Me.PhoneNumberTextBox.Size = New System.Drawing.Size(262, 20)
+        Me.PhoneNumberTextBox.TabIndex = 29
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(24, 140)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(78, 13)
+        Me.Label6.TabIndex = 28
+        Me.Label6.Text = "Phone Number"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(24, 108)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 27
+        Me.Label5.Text = "Date of birth"
+        '
+        'icNumberTextBox
+        '
+        Me.icNumberTextBox.Location = New System.Drawing.Point(119, 50)
+        Me.icNumberTextBox.Name = "icNumberTextBox"
+        Me.icNumberTextBox.Size = New System.Drawing.Size(262, 20)
+        Me.icNumberTextBox.TabIndex = 26
+        '
+        'matricTextBox
+        '
+        Me.matricTextBox.Location = New System.Drawing.Point(119, 19)
+        Me.matricTextBox.Name = "matricTextBox"
+        Me.matricTextBox.Size = New System.Drawing.Size(259, 20)
+        Me.matricTextBox.TabIndex = 25
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 24
+        Me.Label3.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "Ic Number"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 22)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 13)
+        Me.Label4.TabIndex = 22
+        Me.Label4.Text = "Matric Number"
+        '
+        'PreviousButton
+        '
+        Me.PreviousButton.Location = New System.Drawing.Point(45, 195)
+        Me.PreviousButton.Name = "PreviousButton"
+        Me.PreviousButton.Size = New System.Drawing.Size(74, 30)
+        Me.PreviousButton.TabIndex = 32
+        Me.PreviousButton.Text = "Previous"
+        Me.PreviousButton.UseVisualStyleBackColor = True
+        '
+        'AddNewButton
+        '
+        Me.AddNewButton.Location = New System.Drawing.Point(182, 195)
+        Me.AddNewButton.Name = "AddNewButton"
+        Me.AddNewButton.Size = New System.Drawing.Size(74, 30)
+        Me.AddNewButton.TabIndex = 33
+        Me.AddNewButton.Text = "Add New"
+        Me.AddNewButton.UseVisualStyleBackColor = True
+        '
+        'NextButton
+        '
+        Me.NextButton.Location = New System.Drawing.Point(340, 195)
+        Me.NextButton.Name = "NextButton"
+        Me.NextButton.Size = New System.Drawing.Size(74, 30)
+        Me.NextButton.TabIndex = 34
+        Me.NextButton.Text = "Next"
+        Me.NextButton.UseVisualStyleBackColor = True
+        '
+        'SaveButton
+        '
+        Me.SaveButton.Location = New System.Drawing.Point(45, 247)
+        Me.SaveButton.Name = "SaveButton"
+        Me.SaveButton.Size = New System.Drawing.Size(74, 30)
+        Me.SaveButton.TabIndex = 35
+        Me.SaveButton.Text = "Save"
+        Me.SaveButton.UseVisualStyleBackColor = True
+        '
+        'DeleteButton
+        '
+        Me.DeleteButton.Location = New System.Drawing.Point(182, 247)
+        Me.DeleteButton.Name = "DeleteButton"
+        Me.DeleteButton.Size = New System.Drawing.Size(74, 30)
+        Me.DeleteButton.TabIndex = 36
+        Me.DeleteButton.Text = "Delete"
+        Me.DeleteButton.UseVisualStyleBackColor = True
+        '
+        'CloseButton
+        '
+        Me.CloseButton.Location = New System.Drawing.Point(340, 247)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(74, 30)
+        Me.CloseButton.TabIndex = 37
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
         'StudentListForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(1028, 609)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Search)
         Me.Controls.Add(Me.updateStudentButton)
         Me.Controls.Add(Me.AddStudentButton)
@@ -159,6 +338,8 @@ Partial Class StudentListForm
         CType(Me.StudentDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Search.ResumeLayout(False)
         Me.Search.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -173,4 +354,21 @@ Partial Class StudentListForm
     Friend WithEvents Label1 As Label
     Friend WithEvents searchTextBox As TextBox
     Friend WithEvents displayAllButton As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CloseButton As Button
+    Friend WithEvents DeleteButton As Button
+    Friend WithEvents SaveButton As Button
+    Friend WithEvents NextButton As Button
+    Friend WithEvents AddNewButton As Button
+    Friend WithEvents PreviousButton As Button
+    Friend WithEvents dobDateTimePicker1 As DateTimePicker
+    Friend WithEvents nameTextBox As TextBox
+    Friend WithEvents PhoneNumberTextBox As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents icNumberTextBox As TextBox
+    Friend WithEvents matricTextBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
 End Class
